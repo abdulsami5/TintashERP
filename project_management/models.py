dd2e74e5a934a526e7fa4aeaf372156e7145a35f
alizain721 <alizain721@gmail.com>
2018-08-08 21:14:11 +0500

    Create UserProjectRequest Model
    This model will have all the requests made by employees to be added or removed from any project

--- project_management/models.py
+++ project_management/models.py
@@ -8,3 +8,16 @@ class UserProject(models.Model):
     status = models.CharField(max_length=30, blank=True)
     employee = models.ForeignKey(User,on_delete=models.CASCADE)
     project = models.ForeignKey(Project,on_delete=models.CASCADE)
+
+class UserProjectRequest(models.Model):
+    """This is model class is to handle requests of employees to be added  or removed from any project"""
+    employee = models.ForeignKey(User,on_delete=models.CASCADE)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    request_type = models.CharField(max_length =50)
+    status = models.CharField(max_length = 50, blank=True)
+    reason = models.TextField(max_length = 100, blank=True)
+    def save(self, *args, **kwargs):
+        """ Function that converts request_type to lower case at the time of saving it in database"""
+        self.request_type = self.request_type.lower()
+        self.status = self.status.lower()
+        return super(UserProjectRequest, self).save(*args, **kwargs)
