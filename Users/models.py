a9dd386e0d1d699ba20ac12fe28654c57011d28d
Abdul Sami <abdul.sami@tintash.com>
2018-08-08 15:33:46 +0500

    Add User Model Representaion Function
    Add User Model representation function __Str__ in Users/models.py.

--- Users/models.py
+++ Users/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class User(User):
     """This is the User model for database"""
     profile_pic = models.CharField(blank=True, max_length=1000)
     contact_info = models.CharField(blank=True, max_length=30)
+
+
+    def __str__(self):
+        """This function is to get representation of User model"""
+        return self.email
