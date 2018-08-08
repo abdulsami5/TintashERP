5655821d4ba47c9e644b69b954601303c16ea009
alizain721 <alizain721@gmail.com>
2018-08-08 16:01:06 +0500

    Create project get request api
    This api will return all projects,if passed project_id as parameter it will that specific project

--- project/views.py
+++ project/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProjectSerializer
+from .models import Project
 
-# Create your views here.
+class ProjectApiView(APIView):
+    """Project APIview to handle all restfull requests"""
+    def get(self,request, format=None):
+        """This method will return either all projects or a signle proect if passed project_id as parameter"""
+        project_id = request.GET.get('project_id')
+        if project_id:
+            project = Project.objects.filter(id=project_id)
+            if project:
+                project_serializer = ProjectSerializer(project, many=True)
+            else:
+                return Response({"message":"No record found"},status = status.HTTP_404_NOT_FOUND)
+        else:
+            projects = Project.objects.all()
+            project_serializer = ProjectSerializer(projects, many=True)
+        return Response({"data":project_serializer.data})
