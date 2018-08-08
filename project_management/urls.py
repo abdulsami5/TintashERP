6b11210d78134716a83ab60f66bf007f724b8917
alizain721 <alizain721@gmail.com>
2018-08-08 22:08:21 +0500

    Create Api To Get All Requests Made By User
    This api will take user_id as input and will return all requests that were made by him or her

--- project_management/urls.py
+++ project_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('user_projects',views.user_projects),
     path('employee_project_request',views.employee_project_request),
+    path('employee_requests_all',views.employee_requests_all),
 ]
