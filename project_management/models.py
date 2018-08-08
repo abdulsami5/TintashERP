e4222c7c67200dc8e5fda7cd08d60157230983a2
alizain721 <alizain721@gmail.com>
2018-08-08 17:52:50 +0500

    Create UserProject Model
    UserProject model, act as association model between user and projects

--- project_management/models.py
+++ project_management/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserProject(models.Model):
+    """This model is association class between user and projects"""
+    status = models.CharField(max_length=30, blank=True)
+    employee = models.ForeignKey(User,on_delete=models.CASCADE)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
