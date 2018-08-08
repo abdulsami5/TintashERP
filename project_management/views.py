40ddd0a5c52dd5749da9723cb4201258c6416a4e
alizain721 <alizain721@gmail.com>
2018-08-08 21:09:18 +0500

    Create Api To Get Projects of A User
    This api call takes user_id as querry string parameter and returns all the projects related to that particular user

--- project_management/views.py
+++ project_management/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import *
+from .serializers import *
 # Create your views here.
+@csrf_exempt
+@api_view(['GET',])
+def user_projects(request):
+    """This method or api function will return all projects of employee, given employee_id as parameter"""
+    user_id = request.GET.get('user_id')
+    user_projects = UserProject.objects.filter(employee=user_id)
+    user_projects_serializer = UserProjectSerializer(user_projects, many=True)
+    return Response({"user_projects":user_projects_serializer.data},status=status.HTTP_200_OK)
