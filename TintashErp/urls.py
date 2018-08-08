25e9626afbfc7c2ce04fb8e7331836bfb290a801
alizain721 <alizain721@gmail.com>
2018-08-08 17:27:27 +0500

    Add Loghours Urls in Global Urls File
    I have included loghours.urls in global urls file

--- TintashErp/urls.py
+++ TintashErp/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('project.urls')),
     path('auth/', include('rest_framework_social_oauth2.urls')),
+    path('api/',include('loghours.urls')),
 ]
