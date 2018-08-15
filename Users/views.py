be62ca5b7d52167e6223917b620ad62ddbf6d09d
Abdul Sami <abdul.sami@tintash.com>
2018-08-15 15:05:53 +0500

    Implement Function get_user
    Implement function get_user which return user object from request parameter.

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from oauth2_provider.models import Application, AccessToken
 
-# Create your views here.
+
+def get(request):
+    """This function is to get user object from request's access-token"""
+    token_string = request.META.get("HTTP_AUTHORIZATION")
+    if token_string:
+        try:
+            token_string = token_string.split()
+            access_token = AccessToken.objects.get(token=token_string[1])
+            user = User.objects.get(id=access_token.user_id)
+            return user
+        except AccessToken.DoesNotExist, User.DoesNotExist
+            return None
+    else:
+        None
