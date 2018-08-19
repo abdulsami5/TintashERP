2b0e726636a4899290801c821e5f17cc4ac7e2d2
Abdul Sami <abdul.sami@tintash.com>
2018-08-19 20:36:43 +0500

    Remove Unnecessary Parser Classes
    Remove unnecessary parser classes added for swagger, from settings.py.

--- TintashErp/settings.py
+++ TintashErp/settings.py
@@ -101,15 +101,6 @@ REST_FRAMEWORK = {
         # 'oauth2_provider.ext.rest_framework.OAuth2Authentication',  # django-oauth-toolkit < 1.0.0
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # django-oauth-toolkit >= 1.0.0
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
-    ),
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
-        'rest_framework.parsers.JSONParser',
-        'rest_framework_yaml.parsers.YAMLParser',
-    ],
-     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework_yaml.renderers.YAMLRenderer',
     )
 }
 
