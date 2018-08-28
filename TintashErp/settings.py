4c7694493bcfccf5c403633e48fba059c09147e4
Abdul Sami <abdul.sami@tintash.com>
2018-08-28 16:46:35 +0500

    django_heroku Configuration

--- TintashErp/settings.py
+++ TintashErp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -170,3 +171,5 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ['tintash.com']
 
 
 SOCIAL_AUTH_USER_MODEL = 'Users.User'
+
+django_heroku.settings(locals())
